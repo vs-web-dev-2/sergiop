b6d5929c392790696af6c607b596b2ae92453f57
sergio riquelme <rosercori@yahoo.com>
2021-06-09 12:43:09 +0200

    funcionIva

---
+++ funcionIva.js
@@ -0,0 +1,22 @@
+
+const iva=0.21;
+
+function calcularIva(base){
+
+ 
+    let ivaProducto=base*iva;
+    let precioTotal=base+ivaProducto;
+
+    console.log("Base...."+ base);  
+    console.log("iva...."+ iva);  
+    console.log("ivaProducto...."+ ivaProducto);  
+    
+    return precioTotal;
+
+   
+
+}
+
+let precioProducto = calcularIva(200);
+
+console.log("Precio total con iva:...." +  precioProducto );
